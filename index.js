2c2b0a1223067690cd82671b75be625229e91706
Jessica <jess.newman19@gmail.com>
2022-04-08 13:37:55 -0400

    commit tree 04f606a055427c22eaaaaafa337f31f1d50e4066

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// binds an event listener in addingEventListener()
+
+const input = document.getElementById("input");
+
 function addingEventListener() {
-}
+  input.addEventListener("click", () => alert("I was clicked!"))
+};
+
+//need to call the function to invoke it
+addingEventListener();
